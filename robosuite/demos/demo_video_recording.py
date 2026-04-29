0c94372e6f21467d06dc9035939994f2b02ab6ef
cph <cph@smail.nju.edu.cn>
2026-04-29 10:38:40 +0800

    update demo_video.py

--- robosuite/demos/demo_video_recording.py
+++ robosuite/demos/demo_video_recording.py
@@ -57,6 +57,7 @@ if __name__ == "__main__":
         camera_names=camera_names,
         camera_heights=camera_heights,
         camera_widths=camera_widths,
+        hard_reset=False,
     )
 
     obs = env.reset()
